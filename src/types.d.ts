6e4fa7133e64318422b0fed6040ec7d896ab933b
Rubén Santibáñez Acosta <rubensantibanezacosta902@gmail.com>
2024-04-16 20:56:33 +0100

    types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,43 @@
 import { ParsedMail } from "mailparser";
+import { AppendOptions, Box, MailBoxes } from 'imap';
 
+
+export class ImapNow {
+  constructor(imapConfig: ImapConfig);
+
+  public connect(): Promise<void>;
+  public end(): Promise<void>;
+  public destroy(): Promise<void>;
+  public openBox(mailboxName: string): Promise<Box>;
+  public closeBox(mailboxName: string): Promise<void>;
+  public getBoxes(): Promise<MailBoxes>;
+  public addBox(mailboxName: string): Promise<void>;
+  public deleteBox(mailboxName: string): Promise<void>;
+  public renameBox(oldMailboxName: string, newMailboxName: string): Promise<void>;
+  public subscribeBox(mailboxName: string): Promise<void>;
+  public unsubscribeBox(mailboxName: string): Promise<void>;
+  public boxStatus(mailboxName: string): Promise<void>;
+  public getSubscribedBoxes(): Promise<MailBoxes>;
+  public addMailListener(callback: (numNewMsgs: number) => void): void;
+  public removeMailListener(callback: (numNewMsgs: number) => void): void;
+  public addUpdateListener(callback: (seqno: number, info: any) => void): void;
+  public addExpungeListener(callback: (seqno: number) => void): void;
+  public removeExpungeListener(callback: (seqno: number) => void): void;
+  public addCloseListener(callback: () => void): void;
+  public search(criteria: Criteria[]): Promise<number[]>;
+  public fetch(uids: number[], options: FetchOptions): Promise<FetchResult[]>;
+  public copy(source: MessageSource, mailboxName: string): Promise<void>;
+  public move(source: MessageSource, mailboxName: string): Promise<void>;
+  public addFlags(source: MessageSource, flags: any[]): Promise<void>;
+  public delFlags(source: MessageSource, flags: any[]): Promise<void>;
+  public setFlags(source: MessageSource, flags: any[]): Promise<void>;
+  public addKeywords(source: MessageSource, keywords: any[]): Promise<void>;
+  public delKeywords(source: MessageSource, keywords: any[]): Promise<void>;
+  public setKeywords(source: MessageSource, keywords: any[]): Promise<void>;
+  public serverSupports(capability: string): boolean;
+  public expunge(uids?: MessageSource): Promise<void>;
+  public append(msgData: string | Buffer, options: AppendOptions): Promise<void>;
+}
 /**
  * Configuration options for establishing a connection with an IMAP server.
  */
