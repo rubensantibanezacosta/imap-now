e4dde46c69421d2d65af1e184eee59063fe3d480
Rubén Santibáñez Acosta <rubensantibanezacosta902@gmail.com>
2024-08-21 18:56:51 +0100

    delete postinstall

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./imap-now.cjs.production.min.js')
-} else {
-  module.exports = require('./imap-now.cjs.development.js')
-}
